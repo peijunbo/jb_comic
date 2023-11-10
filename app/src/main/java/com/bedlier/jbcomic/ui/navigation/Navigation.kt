f97dae4ec3869e0f5df43d4591d70b6b1def94e6
peijunbo <peijunbo03@gmail.com>
2023-11-10 17:12:57 +0800

    ui: modify drawer

--- app/src/main/java/com/bedlier/jbcomic/ui/navigation/Navigation.kt
+++ app/src/main/java/com/bedlier/jbcomic/ui/navigation/Navigation.kt
@@ -2,20 +2,28 @@ package com.bedlier.jbcomic.ui.navigation
 
 import androidx.compose.animation.core.tween
 import androidx.compose.animation.slideIn
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowRight
 import androidx.compose.material.icons.filled.Error
 import androidx.compose.material.icons.filled.Home
 import androidx.compose.material.icons.filled.Settings
+import androidx.compose.material3.DrawerDefaults
 import androidx.compose.material3.DrawerValue
 import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.ModalDrawerSheet
 import androidx.compose.material3.ModalNavigationDrawer
 import androidx.compose.material3.NavigationDrawerItem
+import androidx.compose.material3.NavigationDrawerItemDefaults
 import androidx.compose.material3.Text
 import androidx.compose.material3.rememberDrawerState
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.compositionLocalOf
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -24,12 +32,17 @@ import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.IntOffset
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
+import androidx.navigation.compose.currentBackStackEntryAsState
+import com.bedlier.jbcomic.R
 import com.bedlier.jbcomic.ui.home.HomeScreen
 import com.bedlier.jbcomic.ui.viewer.ViewerScreen
+import com.elvishew.xlog.XLog
 import kotlinx.coroutines.launch
 
 val drawerDestinations = listOf(
@@ -46,35 +59,55 @@ fun NavContainer() {
     val navController = LocalNavController.current
     val drawerState = rememberDrawerState(initialValue = DrawerValue.Closed)
     val coroutineScope = rememberCoroutineScope()
-    var enableDrawerGestures by remember{ mutableStateOf(false) }
+    val currentBackStackEntry by navController.currentBackStackEntryAsState()
     ModalNavigationDrawer(
         drawerState = drawerState,
-        gesturesEnabled = enableDrawerGestures,
+        gesturesEnabled = when (currentBackStackEntry?.destination?.route) {
+            Screen.Viewer.route -> false
+            else -> true
+        },
         drawerContent = {
-            ModalDrawerSheet {
+            ModalDrawerSheet(
+                modifier = Modifier.fillMaxWidth(0.618f),
+            ) {
+                Text(
+                    text = stringResource(id = R.string.label_navigation),
+                    modifier = Modifier
+                        .padding(NavigationDrawerItemDefaults.ItemPadding)
+                        .padding(top = 16.dp),
+                    fontSize = MaterialTheme.typography.titleMedium.fontSize,
+                    fontStyle = MaterialTheme.typography.titleMedium.fontStyle,
+                    fontWeight = MaterialTheme.typography.titleMedium.fontWeight,
+                )
+                Spacer(modifier = Modifier.height(8.dp))
                 drawerDestinations.forEach { route: String ->
-                    NavigationDrawerItem(label = {
-                        Text(text = route)
-                    }, icon = {
-                        Icon(
-                            imageVector = when (route) {
-                                Screen.Home.route -> Icons.Default.Home
-                                Screen.Settings.route -> Icons.Default.Settings
-                                Screen.About.route -> Icons.Default.Error
-                                else -> Icons.Default.ArrowRight
-                            },
-                            contentDescription = route
-                        )
-                    }, selected = false, onClick = {
-                        coroutineScope.launch { drawerState.close() }
-                        // replace current route
-                        navController.navigate(route) {
-                            popUpTo(0) {
-                                saveState = true
+                    NavigationDrawerItem(
+                        label = {
+                            Text(text = route)
+                        },
+                        icon = {
+                            Icon(
+                                imageVector = when (route) {
+                                    Screen.Home.route -> Icons.Default.Home
+                                    Screen.Settings.route -> Icons.Default.Settings
+                                    Screen.About.route -> Icons.Default.Error
+                                    else -> Icons.Default.ArrowRight
+                                },
+                                contentDescription = route
+                            )
+                        },
+                        selected = route == currentBackStackEntry?.destination?.route,
+                        onClick = {
+                            coroutineScope.launch { drawerState.close() }
+                            // replace current route
+                            navController.navigate(route) {
+                                popUpTo(0) {
+                                    saveState = true
+                                }
+                                restoreState = true
                             }
-                            restoreState = true
                         }
-                    })
+                    )
                 }
             }
         }
@@ -107,5 +140,5 @@ fun NavContainer() {
             }
         }
     }
-    
+
 }
